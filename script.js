703582a351d2043344089ccb0c069a83bd3b427f
tatianamendoza <kairosalvarez94@gmail.com>
2026-04-02 15:14:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const weddingDate = new Date("August 15, 2026 17:00:00").getTime();
 
+const timer = setInterval(function () {
+
+  const now = new Date().getTime();
+  const distance = weddingDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  document.getElementById("timer").innerHTML =
+    days + " días " + hours + " horas " + minutes + " min";
+
+}, 1000);
