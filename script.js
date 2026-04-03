d77de06cebc948cbccf2382e814dd79796ed4a73
tatianamendoza <kairosalvarez94@gmail.com>
2026-04-03 11:24:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -28,8 +28,6 @@ const observer = new IntersectionObserver((entries) => {
       entry.target.classList.add("active");
     }
   });
-}, {
-  threshold: 0.15
-});
+}, { threshold: 0.15 });
 
 reveals.forEach((section) => observer.observe(section));
